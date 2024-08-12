1c10c53cbcda308c67613d3439479d1f59e06cde
fernadnob <ferbastida@hotmail.com>
2024-08-12 20:34:46 -0300

    Arreglo en el html, js y css

--- main.js
+++ main.js
@@ -1,59 +1,97 @@
-let nombreUsuario;
-let edadUsuario;
-const componentesPC = [
-    { nombre: "Procesador", precio: 5000 },
-    { nombre: "Memoria RAM", precio: 3000 },
-    { nombre: "Disco Duro SSD", precio: 2000 }
-];
-let carritoCompras = [];
-let totalCarrito = 0;
+const productos = document.getElementById("productos")
+const carrito = document.getElementById("carrito")
+const Carrito = []
 
+const Productos = [
+    {
+        imagen: "./img/ryzen5.jpg",
+        titulo: "Procesador AMD RYZEN 5 3600 4.2GHz Turbo AM4 Wraith Stealth Cooler",
+        precio: 130000,
+        boton: "Comprar"
+    },
+    {
+        imagen: "./img/ryzen3.jpg",
+        titulo: "Procesador AMD RYZEN 3 3200G 4.0GHz Turbo + Radeon Vega 8 AM4 Wraith Stealth Cooler",
+        precio: 85000,
+        boton: "Comprar"
+    },
+    {
+        imagen: "./img/ryzen71.jpg",
+        titulo: "Procesador AMD Ryzen 7 5700G 4.6GHz Turbo + Wraith Stealth Cooler",
+        precio: 260000,
+        boton: "Comprar"
+    },
+    {
+        imagen: "./img/ryzen72.jpg",
+        titulo: "Procesador AMD Ryzen 7 7700X 5.4GHz Turbo AM5 - No incluye Cooler - C/VIDEO",
+        precio: 465000,
+        boton: "Comprar"
+    },
+    {
+        imagen: "./img/ryzen91.jpg",
+        titulo: "Procesador AMD Ryzen 9 7950X 5.7GHz AM5 - No incluye Cooler - C/Video",
+        precio: 755000,
+        boton: "Comprar"
+    },
+    {
+        imagen: "./img/ryzen92.jpg",
+        titulo: "Procesador AMD Ryzen 9 7950X3D 5.7GHz AM5 - No incluye Cooler -",
+        precio: 892000,
+        boton: "Comprar"
+     }
+]
 
-function interactuarUsuario() {
-    nombreUsuario = prompt("Ingrese su nombre:");
-    edadUsuario = parseInt(prompt("Ingrese su edad:"));
-
-    if (edadUsuario >= 18) {
-        alert("Bienvenido, " + nombreUsuario + "! Puede proceder con la selección de componentes.");
-
-        let mensajeComponentes = "Tipos de componentes disponibles:\n";
-        for (let i = 0; i < componentesPC.length; i++) {
-            const componente = componentesPC[i];
-            mensajeComponentes += (i + 1) + ". " + componente.nombre + " - Precio: " + componente.precio + " pesos\n";
-        }
-
-        let seleccion;
-        do {
-            seleccion = parseInt(prompt(mensajeComponentes + "\nIngrese el número correspondiente al componente que desea añadir al carrito o, ingrese 0 para terminar:"));
-
-            if (seleccion >= 1 && seleccion <= componentesPC.length) {
-                const componenteSeleccionado = componentesPC[seleccion - 1];
-                const cantidad = parseInt(prompt("Ingrese la cantidad de " + componenteSeleccionado.nombre + " que desea comprar:"));
-                if (parseInt(cantidad) && cantidad > 0) {
-                    for (let i = 0; i < cantidad; i++) {
-                        carritoCompras.push(componenteSeleccionado);
-                        totalCarrito += componenteSeleccionado.precio;
-                    }
-                    alert(cantidad + " " + componenteSeleccionado.nombre + " añadido al carrito.");
-                } else {
-                    alert("Cantidad inválida. Por favor, ingrese un número válido mayor a 0.");
-                }
-            } else if (seleccion !== 0) {
-                alert("Selección inválida. Por favor, ingrese un número válido.");
-            }
-        } while (seleccion !== 0);
-
-        let mensajeCarrito = "Resumen de su carrito de compras, " + nombreUsuario + ":\n";
-        for (let i = 0; i < carritoCompras.length; i++) {
-            const componente = carritoCompras[i];
-            mensajeCarrito += componente.nombre + " - Precio: " + componente.precio + " pesos\n";
-        }
-        mensajeCarrito += "Total a pagar: " + totalCarrito + " pesos";
-        alert(mensajeCarrito);
-
-    } else {
-        alert("Lo siento, debe ser mayor de edad para realizar esta acción.");
+const agregadoraAlCarrito = (titulo, precio) => {
+    const booleano = Carrito.some(el =>{
+        return el.titulo === titulo
+    })
+    if(booleano){
+        const productos = Carrito.find(el=> {
+            return el.titulo === titulo
+        })
+    }else{
+        Carrito.push({
+            titulo,
+            precio,
+            cantidad: 1
+        })
     }
 }
 
-interactuarUsuario();
+
+
+
+const creadoraDeCards = (titulo, imagen, precio, boton) => {
+    const contenedor = document.createElement("div")
+    const tituloDOM = document.createElement("h3")
+    const imagenDOM = document.createElement("img")
+    const precioDOM = document.createElement("p")
+    const botonDOM = document.createElement("button")
+    
+    contenedor.classList.add("contenedor")
+    tituloDOM.classList.add("titulo")
+    imagenDOM.classList.add("imagen")
+    precioDOM.classList.add("precio")
+    botonDOM.classList.add("boton")
+
+    tituloDOM.innerText = titulo
+    precioDOM.innerText = "$" + precio
+    imagenDOM.src = imagen
+    botonDOM.innerText = "COMPRAR"
+
+    botonDOM.addEventListener("click", ()=>{
+        agregadoraAlCarrito(titulo, precio)
+    })
+
+    contenedor.appendChild(tituloDOM)
+    contenedor.appendChild(imagenDOM)
+    contenedor.appendChild(precioDOM)
+    contenedor.appendChild(botonDOM)
+
+    return contenedor
+}
+
+Productos.forEach(el => {
+    const productosDOM = creadoraDeCards(el.titulo, el.imagen, el.precio, el.boton)
+    productos.appendChild(productosDOM)
+})
